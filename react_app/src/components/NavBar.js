00efed14433299ef0946fa741818da078689fa7a
amitdas001 <amit.das@wegalvanize.com>
2022-07-06 12:37:20 +0530

    Getting started on navbar

---
+++ react_app/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './NavBar.css'
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
+import DataUsageTwoToneIcon from '@mui/icons-material/DataUsageTwoTone';
+import FeedbackTwoToneIcon from '@mui/icons-material/FeedbackTwoTone';
+
+export default function NavBar() {
+  const [value, setValue] = React.useState(0);
+  return (
+    <Box sx={{ width: 1000 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Home" icon={<HomeTwoToneIcon />} />
+        <BottomNavigationAction label="Data Visualization" icon={<DataUsageTwoToneIcon />} />
+        <BottomNavigationAction label="Feedback" icon={<FeedbackTwoToneIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
