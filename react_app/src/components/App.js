9a28416b70f51ce1b3d745d901ab93137e8e6ea0
amitdas001 <amit.das@wegalvanize.com>
2022-07-13 22:17:44 +0530

    Added stacked area graph

--- react_app/src/components/App.js
+++ react_app/src/components/App.js
@@ -3,16 +3,10 @@ import NavBar from './NavBar';
 import Layout from './Layout';
 import UralsCrude from './UralsCrude';
 
-
-const inlineCSS = {
-  backgroundColor: '#C3C7CE',
-  
-};
-
 function App() {
     return (
       <>
-        <div className="AppBar" style={inlineCSS}>
+        <div className="AppBar">
           <NavBar />
         </div>
         <div className='UralsCrudeOil'>
--- react_app/src/components/Layout.js
+++ react_app/src/components/Layout.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './Layout.css'
 import banner_1 from '../images/banner_1.jpg'
 import { Container, textAlign } from '@mui/system';
 import { Divider } from '@mui/material';
@@ -21,9 +22,22 @@ function Layout() {
         <Divider>
             <p> </p>
         </Divider>
-        <Container style={containerOne}>
-            <img style={imgOne} src={banner_1} alt="BigCo Inc. logo"/>
-        </Container>
+        <div className='rowContent'>
+            <Container style={containerOne}>
+                <img style={imgOne} src={banner_1} alt="BigCo Inc. logo"/>
+            </Container>
+            <Container>
+                <p className='contentFolderOne'>
+                    <span className='defHeading'>
+                        What is Urals Crude Oil?<br/><br/>
+                        <Divider /><br/>
+                    </span>
+                    <span className='contentOne'>
+                        Urals oil is a reference oil brand used as a basis for pricing of the Russian export oil mixture. It is a mix of heavy sour oil of Urals and the Volga region with light oil of Western Siberia. Other reference oils are Brent, West Texas Intermediate and Dubai. Urals brand oil is supplied through the Baku-Novorossiysk pipeline system and the Druzhba pipeline. Urals oil futures trade on Moscow Exchange. There was also an effort to trade it on NYMEX under the name of REBCO (Russian Export Blend Crude Oil); however, not a single trade was made.
+                    </span>
+                </p>
+            </Container>
+        </div>
     </div>
   )
 }
